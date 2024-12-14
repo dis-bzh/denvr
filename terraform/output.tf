81375756bb96faa7642e0247028e2dc492444c56
Vincent Dislaire <vincent@dislaire.bzh>
2024-12-14 17:37:35 +0100

    warren tf provider doesnt expose ip

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,7 @@
 
-resource "local_file" "ansible_inventory" {
-  content = templatefile("${path.module}/inventory.tmpl", {
-    public_ips = warren_floating_ip.ingress.*.public_ipv6
-  })
-  filename = "${path.module}/inventory"
-}
+# resource "local_file" "ansible_inventory" {
+#   content = templatefile("${path.module}/inventory.tmpl", {
+#     public_ips = warren_floating_ip.ingress.*.public_ipv6
+#   })
+#   filename = "${path.module}/inventory"
+# }

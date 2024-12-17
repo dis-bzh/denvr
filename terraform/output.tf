624f0db0330a4b45cab21d6afbd519f5e62da0f9
Vincent Dislaire <vincent@dislaire.bzh>
2024-12-17 21:53:36 +0100

    add ansible deploy in github actions

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,8 @@
 
 resource "local_file" "ansible_inventory" {
   content = templatefile("${path.module}/inventory.tmpl", {
-    public_ips = resource.warren_floating_ip.denvr_ip.*.address
+    public_ips = resource.warren_floating_ip.denvr_ip.*.address,
+    user = var.username
   })
   filename = "${path.module}/inventory"
 }
